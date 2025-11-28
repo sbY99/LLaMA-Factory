fd35f65edb9cdf7c6edf6dec3a99999da8c07c97
sbY99 <didtmdqlsdl@gmail.com>
2025-11-28 08:54:36 +0000

    ADD: dcm training config & inference test code

--- train.sh
+++ train.sh
@@ -1,1 +1,14 @@
-CUDA_VISIBLE_DEVICES=2,3 llamafactory-cli train config/vlm_debug.yaml
+# Debugging
+# CUDA_VISIBLE_DEVICES=2,3 llamafactory-cli train config/vlm_debug.yaml
+
+# Binary Only
+# CUDA_VISIBLE_DEVICES=0,1 llamafactory-cli train config/binary.yaml
+
+# Delta Only
+# CUDA_VISIBLE_DEVICES=0,1 llamafactory-cli train config/delta.yaml
+
+# Postop Only
+CUDA_VISIBLE_DEVICES=0,1 llamafactory-cli train config/postop.yaml
+
+# UI
+# CUDA_VISIBLE_DEVICES=0,1 llamafactory-cli webui
